28fe0111f86c40048f5ec81ae4569d378581f881
Giovanni Carlo Marasco <mexitalian@gmail.com>
2016-04-01 15:20:41 +0700

    Adds jQuery (do not know why it hadn't installed via the generator) Adds basic audio controls via toggle button. Adds static music track from SoundCloud.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+
+let musicManager = function() {
+
+  let soundfile = "../media/hayley.mp3";
+  let audio = new Audio(soundfile);
+  let isPlaying = false;
+  let toggle = function() {
+    if (isPlaying) {
+      audio.pause();
+    }
+    else {
+      audio.play();
+    }
+    isPlaying = !isPlaying;
+    return isPlaying ? "Pause" : "Play";;
+  };
+  return {toggle};
+}();
+
+let $playbackBtn = $(".js-play");
+$playbackBtn.on("click", function(ev) {
+  let newActionText = musicManager.toggle();
+  this.textContent = newActionText;
+});
