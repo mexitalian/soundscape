0e51f096715eddbb8004ea12d0f42f9a80b6f3de
Giovanni Carlo Marasco <mexitalian@gmail.com>
2016-04-02 17:06:18 +0700

    Finishes minimum DOM based viable prototype. Allows scrolling of the waveform and control of Y position of the vessel. Visually allows viewing the ability to avoid colliding with the waveform boundaries.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-
 let musicManager = function() {
 
   let soundfile = "../media/hayley.mp3";
@@ -17,13 +16,84 @@ let musicManager = function() {
       callback.apply({isPlaying});
     }
   };
-  return {toggle, isPlaying};
+  return {audio, toggle, isPlaying};
 }();
 
-let $playbackBtn = $(".js-play");
-$playbackBtn.on("click", function() {
+let waveformManager = function() {
+
+  let src = "../images/spectrum.png";
+  let image = new Image();
+      image.src = src;
+
+  return {
+    src: src,
+    img: image,
+    width: image.width,
+    height: image.height
+  };
+}();
+
+let dom = function() {
+
+  let $playbackBtn = $(".js-play");
+  let $waveContainer = $(".js-wave-container");
+  let $vessel = $(".js-vessel");
+
+  return {$playbackBtn, $waveContainer, $vessel};
+}();
+
+// TMP: hard coded will get these from soundcloud api
+// and from the on the fly getting and creation of the graphic assets
+let audioDuration = 261093;
+let waveformWidth = "82000px";
+
+dom.$playbackBtn.on("click", function() {
+
   let self = this;
+
   musicManager.toggle(function() {
     self.textContent = this.isPlaying ? "Pause" : "Play";
   });
+
+  // if (this.isPlaying) {}
+
+  dom.$waveContainer.velocity({
+    translateX: `-${waveformWidth}`//`transformX(-${waveformManager.width}px)`
+  }, {
+    duration: audioDuration
+  });
 });
+
+
+dom.$waveContainer.css({
+  width: `${waveformManager.width*50}px`,
+  backgroundImage: `url(${waveformManager.src})`
+});
+
+// Desktop
+
+let onKeypress = function(ev) {
+  console.log("upwards");
+  ev.preventDefault();
+  dom.$vessel.velocity("stop").velocity({translateY: "-200"}, {duration: 2500});
+}
+
+$(document).one("keypress", function(ev) {
+  musicManager.toggle();
+  dom.$waveContainer.velocity({
+    translateX: `-${waveformWidth}`//`transformX(-${waveformManager.width}px)`
+  }, {
+    duration: audioDuration
+  });
+  onKeypress(ev);
+});
+
+$(document).on("keyup", ev => {
+  console.log("downwards");
+  $(document).one("keypress", onKeypress);
+  dom.$vessel.velocity("stop").velocity({translateY: "200"}, {duration: 2500});
+});
+
+
+// Need a mobile on tap binding
+
