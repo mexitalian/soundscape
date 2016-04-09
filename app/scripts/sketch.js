cb4bd347d5f3c9d64b4163ff2996861d4529a394
Giovanni Carlo Marasco <mexitalian@gmail.com>
2016-04-09 14:39:18 +0700

    Creates 1 second interval waveform, adds cues to display for the time period of audio. Is a work in progress.

--- app/scripts/sketch.js
+++ app/scripts/sketch.js
@@ -1,33 +1,80 @@
 let sound, fft;
-let WAVEFORM, DURATION; // should be CONST but babel is throwing an error
-let SPEED = 40000; // in milliseconds of audio playback
+let SPEED = 1;
 
 let cnv; // canvas
-let wavePG; // processing graphic
+let wavePG; // processing graphic, n::note graphic is not the way, t::todo remove
+
 let waveWidth;
 let waveXPos;
+let waveManager;
+
+let WaveformManager = function(peaksPerScreen, secondsPerScreen) { // t::todo convert to a class (fun, nth)
+  // We will draw 4 peaks (8 vertices) to screen at any one moment
+  // with 2 peaks (4 verticies) off screen
+
+  // start with 4 always on screen – static
+  let peakDistance = width / peaksPerScreen;
+  let peakResolution = sound.duration() * 4; // t::todo needs to account for parts of a second
+  let peaks = sound.getPeaks(peakResolution); // waveform for full audio <- add resolution here
+  let screenCount = 0;
+
+  let getVertices = function() {
+
+    let vertices = [];
+
+    for (let i=0; i<peaksPerScreen; i++) {
+
+      let j = i + (screenCount * peaksPerScreen);
+      console.log((peakDistance/2) + (i * peakDistance));
+      vertices.push([
+        Math.round((peakDistance/2) + (i * peakDistance)),
+        Math.round( map(peaks[j], -1, 1, height/8, height-(height/8)) )
+      ]);
+    }
+    screenCount++;
+
+    return vertices;
+  };
+  let drawVertices = function() {
+
+    let vertices = getVertices();
+    console.log(vertices);
+    background(0);
+    beginShape();
+    fill(255,0,0);
+    stroke(255,0,0);
+    strokeWeight(1);
+
+    // upper limit
+    for (let v of vertices) {
+      vertex(v[0], v[1] - height/4);
+    }
+
+    // lower limit
+    for (let v of vertices.reverse()) {
+      vertex(v[0], height/4 + v[1]);
+    }
+
+    endShape();
+  };
+
+  // init
+  for (let i=0; i < Math.floor(sound.duration()); i++) {
+    sound.addCue(i, drawVertices);
+  }
+};
 
 function preload() {
-  // put in the soundcloud retrieved URL
-  sound = loadSound('../media/hayley.mp3');
+  sound = loadSound('../media/hayley.mp3'); // put in the soundcloud retrieved URL
 }
 
 function setup() {
 
-  WAVEFORM = sound.getPeaks();
-  DURATION = sound.duration() * 1000;
-
   cnv = createCanvas($(window).width(), 400);
   cnv.mouseClicked(togglePlay);
-
-  waveWidth = width * Math.floor(DURATION / SPEED);
-  let stepWidth = waveWidth / WAVEFORM.length;
   // fft = new p5.FFT();
   // sound.amp(0.2);
-
-  background(0);
-
-  waveXPos = width;//width/2;
+/*
   let waveVertices = [];
   for (let i in WAVEFORM) {
     waveVertices.push([
@@ -35,31 +82,16 @@ function setup() {
       Math.round(map(WAVEFORM[i], -1, 1, 0, height/2))
     ]);
   }
+*/
+  waveManager = new WaveformManager(4, SPEED);
 
-  wavePG = createGraphics(waveWidth, height);
-  wavePG.beginShape();
-  wavePG.fill(255,0,0);
-  wavePG.stroke(255,0,0);
-  wavePG.strokeWeight(1);
-
-  for (let v of waveVertices) {
-    console.log(v[0]);
-    wavePG.vertex(v[0], v[1] - height/8);
-  }
-  for (let v of waveVertices.reverse()) {
-    wavePG.vertex(v[0], height/8 + v[1]);
-  }
-
-  wavePG.endShape();
-
+  background(0);
 }
 
 
 function draw() {
   if (sound.isPlaying()) {
-    background(0);
-    image(wavePG, waveXPos, 0, waveWidth*10, height);
-    waveXPos -= 2.5;
+
   }
 }
 
