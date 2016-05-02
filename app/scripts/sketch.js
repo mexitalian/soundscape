d710181be5f5dac284e71818b423c9d6459b57f8
Giovanni Carlo Marasco <mexitalian@gmail.com>
2016-05-02 20:57:03 +0700

    Cleans up magic numbers and function responsibilities. Almost completely squashed the misplacement of vertices bug, no more flashing. Bug: I think the Math.floor and Math.round functions need to be removed from the calculations as the vertices do not end when the song does.

--- app/scripts/sketch.js
+++ app/scripts/sketch.js
@@ -4,53 +4,72 @@ var sketch = function(p) {
   let fr = 60;
   let SPEED = 1;
   let peaksPerScreen = 4;
+  let peaksPerScreenBuffer = 2;
 
   let cnv; // canvas
-
-  let waveWidth;
   let waveManager;
   let url = player.urls[ Math.floor(Math.random() * player.urls.length) ];
 
-  let WaveformManager = function(peaksPerScreen, secondsPerScreen) { // t::todo convert to a class (fun, nth)
+  let WaveformManager = function(sound, peaksPerScreen, secondsPerScreen) { // t::todo convert to a class (fun, nth)
+
+    let waveWidth = Math.floor(sound.duration() * p.width);
     // We will draw 4 peaks (8 vertices) to screen at any one moment
     // with 2 peaks (4 verticies) off screen
-
     // start with 4 always on screen – static
     let peakDistance = p.width / peaksPerScreen;
+    let frameDistance = p.width / fr;
     let peakResolution = sound.duration() * 4; // t::todo needs to account for parts of a second
     let peaks = sound.getPeaks(peakResolution); // waveform for full audio <- add resolution here
     // let screenCount = 0;
+    let positionX = 0;
     let offsetX = 0;
     let vertices = [];
 
     let updateOffsetX = function() {
-      offsetX = Math.floor( (offsetX + (p.width / fr)) % peakDistance );
+      offsetX = Math.round( positionX % peakDistance );
+      // offsetX = Math.round( (offsetX + frameDistance) % peakDistance );
       console.log(offsetX);
     };
 
-    let getVertices = function() {
+    let updatePositionX = function() {
+      if (positionX < waveWidth) {
+        // move one full screen per framerate
+        positionX += Math.floor(frameDistance);
+      }
+      else {
+        positionX = 0;
+      }
+    };
+
+    let updateVertices = function() {
 
       vertices = [];
 
-      for (let i=0; i<peaksPerScreen+2; i++) {
+      for (let i=0; i < peaksPerScreen+peaksPerScreenBuffer; i++) {
 
         let j = i + Math.floor(positionX/peakDistance);
+        console.log(j);
 
         vertices.push([
           Math.floor(i * peakDistance),//Math.round((peakDistance/2) + (i * peakDistance)),
           Math.round( p.map(peaks[j], -1, 1, p.height/8, p.height-(p.height/8)) )
         ]);
-      }
+      };
 
       // t::todo put this line in
       // return the vertices from this function to wherever it is called
       // return vertices;
     };
 
+    let updateVars = function() {
+      updateVertices();
+      updateOffsetX();
+      updatePositionX();
+    };
+
     this.draw = function() {
 
-      getVertices();
-      updateOffsetX();
+      updateVars();
 
       p.background(0);
       p.beginShape();
@@ -77,11 +96,14 @@ var sketch = function(p) {
 
     // cue points
     // for (let i=0; i < Math.floor(sound.duration()); i++) {
-    //   sound.addCue(i, getVertices);
+    //   sound.addCue(i, updateVertices);
     // }
   };
 
+
+
 /*
+  -----------------------
   Processing Loop - start
   -----------------------
 */
@@ -98,8 +120,7 @@ var sketch = function(p) {
     cnv.mouseClicked(togglePlay);
     // fft = new p5.FFT();
     // sound.amp(0.2);
-    waveWidth = Math.floor(sound.duration() * p.width);
-    waveManager = new WaveformManager(peaksPerScreen, SPEED);
+    waveManager = new WaveformManager(sound, peaksPerScreen, SPEED);
 
     p.background(0);
   };
@@ -107,24 +128,16 @@ var sketch = function(p) {
   p.draw = function() {
     if (sound.isPlaying()) {
       waveManager.draw();
-      updatePositionX();
     }
   };
 
 /*
-  Processing Loop - end
-  ---------------------
+  -----------------------
+  Processing Loop -  end
+  -----------------------
 */
 
-  let positionX = 0;
-  let updatePositionX = function() {
-    if (positionX<waveWidth) {
-      positionX += Math.floor(p.width / fr);
-    }
-    else {
-      positionX = 0;
-    }
-  };
+
 
   // fade sound if mouse is over canvas
   let togglePlay = function() {
